535df2f4a1452ec40e080240785ac52d8a0c16c6
Luan Marques <luanmarques1789@gmail.com>
2022-08-14 16:41:08 -0300

    .dockerignore file added and API's Dockerfile improved

--- api/db/script.sql
+++ api/db/script.sql
@@ -1,12 +1,17 @@
 CREATE DATABASE IF NOT EXISTS `api`;
+
 USE `api`;
 
-CREATE TABLE products (
+CREATE TABLE IF NOT EXISTS `products` (
   id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
   name VARCHAR(255) NOT NULL,
   price DECIMAL(10,2) NOT NULL
 );
 
-INSERT INTO products VALUES (NULL, 'Product 1', 10.00), 
-  (NULL, 'Product 2', 20.00), 
-  (NULL, 'Product 3', 30.00);
+INSERT INTO `products` VALUES (NULL, 'Generic Gaming Mouse', 14.99), 
+  (NULL, 'HP DeskJet 2755e Wireless ', 94.99), 
+  (NULL, 'Lenovo Ideapad 3 Laptop', 386.99), 
+  (NULL, 'Professional Thin 75Hz 1080p LED Monitor ', 103.66), 
+  (NULL, 'Nintendo Switch Pro Controller', 69.95), 
+  (NULL, 'Racing Style Gaming Chair', 145.00), 
+  (NULL, 'RGB Gaming Keyboard', 48.99);
